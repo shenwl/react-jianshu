be731d9af5b5bfcf9430425a2e883227891ff13e
shenwl <shenweilun96@gmail.com>
2018-07-20 22:52:05 +0800

    从index间接应用headerReducer

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux'
-import headerReducer from '../common/header/store/reducer'
+import { reducer as headerReducer } from '../common/header/store/'
 
 export default combineReducers({
   header: headerReducer
