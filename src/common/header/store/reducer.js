75234de8d1debe9d070c3dacd323143b3085b6a2
shenwl <shenweilun96@gmail.com>
2018-07-21 22:30:22 +0800

    使用immutable.js

--- src/common/header/store/reducer.js
+++ src/common/header/store/reducer.js
@@ -1,20 +1,19 @@
 import * as actionTypes from './actionTypes'
+import { fromJS } from 'immutable'
 
 
-const defaultState = {
+const defaultState = fromJS({
   focused: false
-}
+})
 
 export default (state = defaultState, action) => {
   if(action.type === actionTypes.SEARCH_INPUT_FOCUS) {
-    const newState = JSON.parse(JSON.stringify(state))
-    newState.focused = true
-    return newState
+    return state.set('focused', true)
   }
   if(action.type === actionTypes.SEARCH_INPUT_BLUR) {
-    const newState = JSON.parse(JSON.stringify(state))
-    newState.focused = false
-    return newState
+    // const newState = JSON.parse(JSON.stringify(state))
+    // newState.focused = false
+    return state.set('focused', false)
   }
   return state
 }
