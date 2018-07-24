a536d2597720512609b254133ee42e80ed8bb972
shenwl <shenweilun96@gmail.com>
2018-07-24 20:24:40 +0800

    调整样式

--- src/pages/home/style.js
+++ src/pages/home/style.js
@@ -46,26 +46,31 @@ export const TopicItem = styled.div`
 `
 export const ListWrapper = styled.div`
   display: flex;
-  flex-derection: column;
+  flex-direction: column;
   align-items: flex-start;
+  border-top: 1px solid #f0f0f0;
 `
 export const ListItem = styled.div`
   height: 124px;
   padding: 12px 0 12px 0;
-  border-top: 1px solid #f0f0f0;
   border-bottom: 1px solid #f0f0f0;
   display: flex;
   justify-content: space-between;
   flex-wrap: nowrap;
   .article-pic {
+    display: block;
     width: 120px;
     height: 100%;
+    border-radius: 10px;
   }
 `
 export const ListInfo = styled.div`
+  width: 500px;
   .article-info-title {
     font-size: 19px;
     line-height: 1.5;
+    color: #333;
+    font-weight: bold;
   }
   .article-info-summary {
     font-size: 13px;
