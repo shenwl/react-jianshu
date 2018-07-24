5a5c241bb97ff86568164545bb0a76b06f1f1508
shenwl <shenweilun96@gmail.com>
2018-07-24 15:41:52 +0800

    Topic区域布局

--- src/pages/home/components/Topic.js
+++ src/pages/home/components/Topic.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react'
+import { TopicWrapper, TopicItem } from '../style'
 
 
 class Topic extends Component {
   render() {
     return (
-      <div>
-        Topic
-      </div>
+      <TopicWrapper>
+        <TopicItem>
+          <img className="topic-name-img" src="//upload.jianshu.io/collections/images/75/22.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/64/h/64" />
+          科技美学
+        </TopicItem>
+        <TopicItem>
+          <img className="topic-name-img" src="//upload.jianshu.io/collections/images/75/22.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/64/h/64" />
+          生活大爆炸
+        </TopicItem>
+        <TopicItem>
+          <img className="topic-name-img" src="//upload.jianshu.io/collections/images/75/22.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/64/h/64" />
+          动漫
+        </TopicItem>
+        <TopicItem>
+          <img className="topic-name-img" src="//upload.jianshu.io/collections/images/75/22.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/64/h/64" />
+          前端
+        </TopicItem>
+        <TopicItem>
+          <img className="topic-name-img" src="//upload.jianshu.io/collections/images/261938/man-hands-reading-boy-large.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/64/h/64" />
+          社会热点
+        </TopicItem>
+      </TopicWrapper>
     )
   }
 }
--- src/pages/home/style.js
+++ src/pages/home/style.js
@@ -8,15 +8,39 @@ export const HomeWrapper = styled.div`
   flex-wrap: nowrap;
 `
 export const HomeLeft = styled.div`
-  width: 625px;
+  width: 640px;
   padding-left: 15px;
   padding-top: 30px;
   .banner-img {
-    width: 625px;
+    width: 100%;
     height: 240px;
   }
 `
 export const HomeRight = styled.div`
   padding-top: 30px;
   width: 270px;
+`
+export const TopicWrapper = styled.div`
+  padding: 20px 120px 0 0;
+  display: flex;
+  flex-wrap: wrap;
+`
+export const TopicItem = styled.div`
+  flex-shrink: 0;
+  padding-right: 10px;
+  margin-bottom: 20px;
+  height: 32px;
+  line-height: 32px;
+  margin-right: 20px;
+  background: #f7f7f7;
+  font-size: 14px;
+  border: 1px solid #dcdcdc;
+  border-radius: 4px;
+  .topic-name-img {
+    display: block;
+    float: left;
+    width: 30px;
+    height: 30px;
+    margin-right: 10px;
+  }
 `
