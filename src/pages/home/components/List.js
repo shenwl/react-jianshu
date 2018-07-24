0d7ead25b7ee25ace3ef298a62f5e1a0a7d9ba4c
shenwl <shenweilun96@gmail.com>
2018-07-24 17:14:22 +0800

    ListItem 样式

--- src/pages/home/components/List.js
+++ src/pages/home/components/List.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react'
+import { 
+  ListWrapper,
+  ListItem,
+  ListInfo } from '../style'
 
 
 class List extends Component {
   render() {
     return (
-      <div>
-        List
-      </div>
+      <ListWrapper>
+        <ListItem>
+          <ListInfo>
+            <h3 className="article-info-title">玩转职场，你还差一个项目管理思维</h3>
+            <p className="article-info-summary">PMI（美国项目管理协会）发布的第10次薪酬调查报告显示，PMP（项目管理专业人士）证书是影响项目管理专业人士薪酬位数的重要原因。持有PMP证书...</p>
+          </ListInfo>
+          <img className="article-pic" src="//upload-images.jianshu.io/upload_images/3052965-f8cfc95f95487cc0.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/300/h/240"/>
+        </ListItem>
+      </ListWrapper>
     )
   }
 }
--- src/pages/home/style.js
+++ src/pages/home/style.js
@@ -44,3 +44,32 @@ export const TopicItem = styled.div`
     margin-right: 10px;
   }
 `
+export const ListWrapper = styled.div`
+  display: flex;
+  flex-derection: column;
+  align-items: flex-start;
+`
+export const ListItem = styled.div`
+  height: 124px;
+  padding: 12px 0 12px 0;
+  border-top: 1px solid #f0f0f0;
+  border-bottom: 1px solid #f0f0f0;
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: nowrap;
+  .article-pic {
+    width: 120px;
+    height: 100%;
+  }
+`
+export const ListInfo = styled.div`
+  .article-info-title {
+    font-size: 19px;
+    line-height: 1.5;
+  }
+  .article-info-summary {
+    font-size: 13px;
+    line-height: 24px;
+    color: #999;
+  }
+`
