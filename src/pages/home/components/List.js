e24ed092fc0085cca75b7e36dca90b70e9d17f40
shenwl <shenweilun96@gmail.com>
2018-07-24 20:36:46 +0800

    文章列表

--- src/pages/home/components/List.js
+++ src/pages/home/components/List.js
@@ -3,22 +3,40 @@ import {
   ListWrapper,
   ListItem,
   ListInfo } from '../style'
+import { connect } from 'react-redux'
 
 
 class List extends Component {
   render() {
+    const { articleList } = this.props
     return (
       <ListWrapper>
-        <ListItem>
-          <ListInfo>
-            <h3 className="article-info-title">玩转职场，你还差一个项目管理思维</h3>
-            <p className="article-info-summary">PMI（美国项目管理协会）发布的第10次薪酬调查报告显示，PMP（项目管理专业人士）证书是影响项目管理专业人士薪酬位数的重要原因。持有PMP证书...</p>
-          </ListInfo>
-          <img className="article-pic" src="//upload-images.jianshu.io/upload_images/3052965-f8cfc95f95487cc0.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/300/h/240"/>
-        </ListItem>
+        {
+          articleList.map(article => (
+            <ListItem key={article.get('id')}>
+              <ListInfo>
+                <h3 className="article-info-title">{article.get('title')}</h3>
+                <p className="article-info-summary">{article.get('summary')}</p>
+              </ListInfo>
+              <img className="article-pic" src={article.get('imgUrl')} alt=''/>
+            </ListItem>
+          ))
+        }
       </ListWrapper>
     )
   }
 }
 
-export default List
+const mapStateToProps = (state) => {
+  return {
+    articleList: state.getIn(['home', 'articleList']),
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+  }
+}
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(List)
--- src/pages/home/store/reducer.js
+++ src/pages/home/store/reducer.js
@@ -15,6 +15,26 @@ const defaultState = fromJS({
       imgUrl: '//upload.jianshu.io/collections/images/14/6249340_194140034135_2.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/64/h/64'
     },
   ],
+  articleList: [
+    {
+      id: 1,
+      title: '玩转职场，你还差一个项目管理思维',
+      summary: 'PMI（美国项目管理协会）发布的第10次薪酬调查报告显示，PMP（项目管理专业人士）证书是影响项目管理专业人士薪酬位数的重要原因。持有PMP证书...',
+      imgUrl: '//upload-images.jianshu.io/upload_images/3052965-f8cfc95f95487cc0.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/300/h/240',
+    },
+    {
+      id: 2,
+      title: '玩转职场，你还差一个项目管理思维',
+      summary: 'PMI（美国项目管理协会）发布的第10次薪酬调查报告显示，PMP（项目管理专业人士）证书是影响项目管理专业人士薪酬位数的重要原因。持有PMP证书...',
+      imgUrl: '//upload-images.jianshu.io/upload_images/3052965-f8cfc95f95487cc0.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/300/h/240',
+    },
+    {
+      id: 3,
+      title: '玩转职场，你还差一个项目管理思维',
+      summary: 'PMI（美国项目管理协会）发布的第10次薪酬调查报告显示，PMP（项目管理专业人士）证书是影响项目管理专业人士薪酬位数的重要原因。持有PMP证书...',
+      imgUrl: '//upload-images.jianshu.io/upload_images/3052965-f8cfc95f95487cc0.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/300/h/240',
+    },
+  ],
 })
 
 export default (state = defaultState, action) => {
