2f7594c14fe773fb662a51e18cc7c91eb64d453a
shenwl <shenweilun96@gmail.com>
2018-07-25 23:14:00 +0800

    优化reducer代码

--- src/pages/home/store/reducer.js
+++ src/pages/home/store/reducer.js
@@ -10,19 +10,27 @@ const defaultState = fromJS({
   showGoTop: false,
 })
 
+const changeHomeData = (state, action) => {
+  return state.merge({
+    topicList: fromJS(action.topicList),
+    articleList: fromJS(action.articleList),
+    recommendList: fromJS(action.recommendList),
+  })
+}
+
+const addArticleList = (state, action) => {
+  return state.merge({
+    articleList: state.get('articleList').concat(action.list),
+    articlePage: action.nextPage,
+  })
+}
+
 export default (state = defaultState, action) => {
   switch(action.type) {
     case actionTypes.CHANGE_HOME_DATA:
-      return state.merge({
-        topicList: fromJS(action.topicList),
-        articleList: fromJS(action.articleList),
-        recommendList: fromJS(action.recommendList),
-      })
+      return changeHomeData(state, action)
     case actionTypes.ADD_ARTICLE_LIST:
-      return state.merge({
-        articleList: state.get('articleList').concat(action.list),
-        articlePage: action.nextPage,
-      })
+      return addArticleList(state, action)
     case actionTypes.TOGGLE_GOTOP_SHOW:
       return state.set('showGoTop', action.show)
     default:
