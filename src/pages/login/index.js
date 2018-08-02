4526c2dfba9405048828ae5b44bdc936659f04e0
shenwl <shenweilun96@gmail.com>
2018-08-02 20:45:12 +0800

    Redirect 登录状态跳转到首页

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,5 +1,6 @@
 import React, { PureComponent } from 'react'
 import { connect } from 'react-redux'
+import { Redirect } from 'react-router-dom'
 import { actionCreators } from './store'
 import { 
   LoginLayout,
@@ -9,24 +10,31 @@ import {
 
 class Login extends PureComponent {
   render() {
-    const { login } = this.props
+    const { login, handleLogin } = this.props
+    if(login) {
+      return (
+        <Redirect to="/" />
+      )
+    }
     return (
       <LoginLayout>
         <LoginBox>
           <Input placeholder="请输入账号" type="text" innerRef={input => { this.accountEl = input }} />
           <Input placeholder="请输入密码" type="password" innerRef={input => { this.passwordEl = input }} />
-          <Button onClick={() => { login(this.accountEl, this.passwordEl) }}>登录</Button>
+          <Button onClick={() => { handleLogin(this.accountEl, this.passwordEl) }}>登录</Button>
         </LoginBox>
       </LoginLayout>
     )
   }
 }
 
-const mapStateToProps = null
+const mapStateToProps = (state) => ({
+  login: state.getIn(['login', 'login']),
+})
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    login(accountEl, passwordEl) {
+    handleLogin(accountEl, passwordEl) {
       dispatch(actionCreators.login(accountEl.value, passwordEl.value))
     },
   }
